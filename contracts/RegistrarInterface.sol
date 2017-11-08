f49abb9c100af49a978698a5a129e18559497555
Nick Johnson <arachnid@notdot.net>
2017-11-08 00:09:17 -0500

    Interface tweaks

--- contracts/RegistrarInterface.sol
+++ contracts/RegistrarInterface.sol
@@ -1,13 +1,14 @@
 pragma solidity ^0.4.0;
 
 contract RegistrarInterface {
-  event OwnerChanged(bytes32 indexed name, address indexed oldOwner, address indexed newOwner);
-  event DomainConfigured(bytes32 indexed name);
-  event DomainUnlisted(bytes32 indexed name);
-  event NewRegistration(bytes32 indexed name, string label, address indexed owner, address indexed referrer, uint price);
+  event OwnerChanged(bytes32 indexed label, address indexed oldOwner, address indexed newOwner);
+  event DomainConfigured(bytes32 indexed label);
+  event DomainUnlisted(bytes32 indexed label);
+  event NewRegistration(bytes32 indexed label, string subdomain, address indexed owner, address indexed referrer, uint price);
+  event RentPaid(bytes32 indexed label, string subdomain, uint amount, uint expirationDate);
 
-  function query(bytes32 label, string subdomain) view returns(string domain, uint price, uint rent, uint referralFeePPM);
-  function register(bytes32 label, string subdomain, address subdomainOwner, address referrer) public payable;
+  function query(bytes32 label, string subdomain) view returns(string domain, uint signupFee, uint rent, uint referralFeePPM);
+  function register(bytes32 label, string subdomain, address owner, address referrer) public payable;
   function supportsInterface(bytes4 interfaceID) constant returns (bool);
 
   function rentDue(bytes32 label, string subdomain) public view returns(uint timestamp);
