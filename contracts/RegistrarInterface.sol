87dc73486136a0ebde8bf223546bdca2ed350a31
Nick Johnson <arachnid@notdot.net>
2017-11-15 14:21:32 +0000

    Add comment re interfaceID

--- contracts/RegistrarInterface.sol
+++ contracts/RegistrarInterface.sol
@@ -7,6 +7,7 @@ contract RegistrarInterface {
   event NewRegistration(bytes32 indexed label, string subdomain, address indexed owner, address indexed referrer, uint price);
   event RentPaid(bytes32 indexed label, string subdomain, uint amount, uint expirationDate);
 
+  // InterfaceID of these four methods is 0xc898e728
   function query(bytes32 label, string subdomain) view returns(string domain, uint signupFee, uint rent, uint referralFeePPM);
   function register(bytes32 label, string subdomain, address owner, address referrer) public payable;
 
