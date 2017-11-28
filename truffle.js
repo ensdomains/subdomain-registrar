4f03c2aa2c0e56093f788a7604765d5522ba14cd
Dean Eigenmann <dean@tokenate.io>
2017-11-28 12:50:34 +0100

    Migrate/trufflev4 (#25)
    * updated to truffle v4 so we can start using travis
    
    * updated

--- truffle.js
+++ truffle.js
@@ -1,11 +1,3 @@
 require('babel-register');
 
-module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
-};
+module.exports = { };
