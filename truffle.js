82ad40e633cda137dba94a1218d58e654596624f
Rocky Fikki <rocky@fikki.net>
2017-12-02 08:14:08 -0600

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,3 +1,13 @@
 require('babel-register');
 
-module.exports = { };
+module.exports = {
+    networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            gas: 4600036,
+            network_id: "*" // Match any network id
+        }
+    }
+};
+
