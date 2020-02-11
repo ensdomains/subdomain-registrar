1d4a64fa6df138e77bce4e5d8cde8ca1389a02e5
Nick Johnson <arachnid@notdot.net>
2020-02-12 11:30:26 +1300

    Update with ENS address [ci skip]

--- test/Dependencies.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.0;
-
-import "@ensdomains/ens/contracts/ENSRegistry.sol";
-import "@ensdomains/ens/contracts/HashRegistrar.sol";
-import "@ensdomains/ethregistrar/contracts/BaseRegistrarImplementation.sol";
-
-contract Dependencies {
-    constructor() public { }
-}
